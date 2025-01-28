47a5cb75330a6445a73e35058f57fa87866d7905
Kehinde-Oluwasogo <Kehinde.oluwasogo@ada.ac.uk>
2025-01-28 16:49:40 +0000

    This passes the test when im not throwing an error while a product class is being instantiated. i will be throwing an error and adjusting the tests in the next commit.

--- primer4/Product.js
+++ primer4/Product.js
@@ -5,10 +5,10 @@ class Product {
     #quantity;
   
     constructor(id, name, price, quantity) {
-      if (new.target === Product) {
-        // if the class being instantiated is Product throw error
-        throw new Error("Cannot instantiate abstract class Product directly.");
-      }
+      // if (new.target === Product) {
+      //   // if the class being instantiated is Product throw error
+      //   throw new Error("Cannot instantiate abstract class Product directly.");
+      // }
       this.#id = id;
       this.#name = name;
       this.#price = price;
